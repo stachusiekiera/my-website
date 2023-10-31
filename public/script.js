ce3d6c6940f0d79fa52fc26a627977ca1e62edef
stachusiekiera <stonytark97@gmail.com>
2023-10-31 12:09:13 +0100

    Updated search bar in HTML

---
+++ public/script.js
@@ -0,0 +1,5 @@
+document.querySelector('.search-btn').addEventListener('click', function() {
+    const query = document.querySelector('.search-bar input').value;
+    console.log("Searching for:", query);
+    // You can replace the above line with any functionality you want.
+});
