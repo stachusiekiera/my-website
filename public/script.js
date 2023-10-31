285bda92b0c33314010d3c5461ff56d1f0580a38
stachusiekiera <stonytark97@gmail.com>
2023-10-31 13:00:41 +0100

    Updated script.js and other changes

--- public/script.js
+++ public/script.js
@@ -1,5 +1,19 @@
+// Handle search button click
 document.querySelector('.search-btn').addEventListener('click', function() {
     const query = document.querySelector('.search-bar input').value;
     console.log("Searching for:", query);
     // You can replace the above line with any functionality you want.
+});
+
+// Handle focus and blur (focusout) events on the search bar
+const searchContainer = document.querySelector('.search-bar');
+
+searchContainer.addEventListener('focusin', () => {
+    searchContainer.style.width = "320px";
+    searchContainer.style.backgroundColor = "#e8e8e8";
+});
+
+searchContainer.addEventListener('focusout', () => {
+    searchContainer.style.width = "300px";
+    searchContainer.style.backgroundColor = "#f5f5f5";
 });
