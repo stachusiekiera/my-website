3ecdeffa7f87d17a7257411b5d68eeb7d82e5464
stachusiekiera <stonytark97@gmail.com>
2023-10-31 20:11:36 +0100

    Add purple focus style to input

--- functions/askGPT.js
+++ functions/askGPT.js
@@ -2,8 +2,13 @@ const axios = require('axios');
 
 exports.handler = async function(event, context) {
     try {
-        const response = await axios.post("https://api.openai.com/v1/engines/text-davinci-003/completions", {
-            // ... your request body here ...
+        const response = await axios.post("https://api.openai.com/v1/chat/completions", {
+            model: "gpt-3.5-turbo",
+            messages: [{
+                role: "user",
+                content: "Say this is a test!"
+            }],
+            temperature: 0.7
         }, {
             headers: {
                 'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
