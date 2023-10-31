cb21ed372a3ce189ff882a9a3de32dd4c9505ce9
stachusiekiera <stonytark97@gmail.com>
2023-10-31 18:22:07 +0100

    Add purple focus style to input

--- functions/askGPT.js
+++ functions/askGPT.js
@@ -1,44 +1,32 @@
-const axios = require('axios');
+const { Configuration, OpenAIApi } = require("openai");
 
 exports.handler = async function(event, context) {
-  if (event.httpMethod !== "POST") {
-    return { statusCode: 405, body: "Method Not Allowed" };
-  }
-
-  const { question } = JSON.parse(event.body);
-
-  try {
-    const response = await axios.post('https://api.openai.com/v1/chat/completions', {
-      model: "gpt-3.5-turbo",
-      messages: [
-        {role: "user", content: question}
-      ]
-    }, {
-      headers: {
-        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-        'Content-Type': 'application/json'
-      }
-    });
-
-    // Assuming the response structure is as per the provided documentation:
-    const answer = response.data.choices[0].message.content.trim();
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ answer: answer })
-    };
-
-  } catch (error) {
-    console.error("Error:", error);
-    
-    // Log detailed error message if it's from axios
-    if (error.response) {
-      console.error("Detailed Error:", error.response.data);
+    if (event.httpMethod !== "POST") {
+        return { statusCode: 405, body: "Method Not Allowed" };
     }
 
-    return {
-        statusCode: 500,
-        body: JSON.stringify({ error: error.message })
-    };
-  }
+    const { question } = JSON.parse(event.body);
+    const configuration = new Configuration({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+
+    try {
+        const response = await openai.createCompletion({
+            engine: "davinci",
+            prompt: question,
+            max_tokens: 150
+        });
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ answer: response.data.choices[0].text.trim() })
+        };
+    } catch (error) {
+        console.error("Error:", error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message })
+        };
+    }
 };
